55d6c6c09bf991c7f4e58a6bd3afe5aaf2bc9243
user <user@DESKTOP-SI1MC9N>
2020-11-27 11:49:55 +0900

    20201127 (MVC) index.jsp 시작

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -22,12 +22,25 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * 스래시루트 / 라는 이름으로 요청 받으면, home.jsp파일에 render(렌더링-화면출력) 됩니다.아래
+	 * 외부(웹브라우저)에서 스래시루트 / 라는 이름으로 요청 받으면, home.jsp파일에 render(렌더링-화면출력) 됩니다.아래
 	 * HomeController.java 를 저장하면,이클립스에서는 HomeController.class 실행가능한 파일을 생성
 	 * 그래서,저장시 콘솔에 보시면, Reloading Context with... is completed 재실행 되었다고 나옵니다.
 	 * 그래서,.java 클래스는 수정 후 약간 기다린 후 Reloading... 메세지 후 결과 확인이 가능합니다.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	@RequestMapping(value="/",method=RequestMethod.GET) //여기가 절대경로.
+	public String index() {
+		//매서드(함수)의 구성: 리턴(반환값출력)형태지정 매서드 명(매개변수) {구현내용}=> string index(){}
+		//상단 리퀘스트매핑에서 지정한 경로로 접근을 하게되면, 아래에 있는 index()메서드를 실행하라.
+		//매서드명 제일 앞에 있는 public는 접근권한에 대한 키워드인데요, public(공공-다른클래스에서 접근가능), private(내부-현재 HomeController에서만 접근가능)
+		//public String index() {} 매서드 기본형태
+		//$(document).ready(function(){  });  j쿼리 기본형태
+		//리턴반환 값으로 사용한 String 클래스는 문자열을 반환하고, 여기서는 index.jsp에 매칭되는 문자열을 반환.
+		//위에 index.jsp는 상대경로가 필요합니다. 스프링 MVC 프로젝트에서 상대경로의 최상위는 views폴더 최상위 root입니다.
+		//접두사(prefix), 접미사(suffix) -> index(접두사-파일명).jsp(접미사-확장자)
+		return "sample/index"; //접두사만 반환값으로 넣습니다. & 여기가 상대경로.
+	}
+    /*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("개발자들이 변수 값 확인용으로 사용하는 것이 로그입니다. 환영합니다. 현재 여러분 컴퓨터언어는 {} 입니다.", locale);
 		logger.info("환영합니다. 현재 여러분의 컴퓨터 언어는 " + locale + "입니다.");
@@ -46,6 +59,6 @@ public class HomeController {
 		return "home";//결과적으로 return 출력값이 home(생략.jsp)에 연동됩니다.
 		//스프링에서 뷰단으로 데이터 이동(전송)에 대해서 살펴보았습니다.
 	
-	}
+	}*/
 	
 }
