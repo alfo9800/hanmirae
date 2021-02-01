8f851290cd21502aa856296d9f559127ceafc2c6
user <user@DESKTOP-SI1MC9N>
2021-02-01 17:07:10 +0900

    Junit 1초 증가 부분 마무리 전.

--- src/test/java/kr/or/test/HelloWorldApp.java
+++ src/test/java/kr/or/test/HelloWorldApp.java
@@ -1,9 +1,23 @@
 package kr.or.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		// 위 main()메서드는 자바프로그램의 실행 진입지점입니다.
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd HHmmss");
+		Date reg_date = new Date();
+		//boardVO.setBno(프라이머리키);
+		for(int cnt=0;cnt<=10;cnt++) {//더미게시물 100입력
+			Calendar cal = Calendar.getInstance();//+
+			cal.setTime(reg_date);//+
+			cal.add(Calendar.SECOND, cnt);//+ cnt초 더하기
+			System.out.println(format.format(cal.getTime()));
+		}
+			// 위 main()메서드는 자바프로그램의 실행 진입지점입니다.
+		
 		System.out.println("헬로우월드!");
 		//plusFunc(3,4); //플러스 함수를 실행 할 때 매개변수 값을 줍니다.
 		
